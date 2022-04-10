85495ac2032b3ea794550ae9e950b006eadf433c
Jars-Bit <79582789+Jars-Bit@users.noreply.github.com>
2022-04-10 18:02:36 -0400

    Add files via upload

---
+++ src/App/Crud.sql
@@ -0,0 +1,14 @@
+create database Crud;
+
+use Crud;
+
+create table Clientes (
+	id int,
+	nombre_Usuario varchar(100),
+	nombre varchar(100),
+	apellido varchar(100),
+	Correo varchar(100),
+	Numero_telefono varchar(100),
+	pass varchar(100),
+	pass_Conf varchar(100)
+);
